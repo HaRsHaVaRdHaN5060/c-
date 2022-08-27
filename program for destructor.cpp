1bdf6b447bf7ef0894b4f8b4d9ddcd6508d5f21d
HaRsHaVaRdHaN5060 <112234806+HaRsHaVaRdHaN5060@users.noreply.github.com>
2022-08-27 14:12:54 +0530

    Add files via upload

---
+++ program for destructor.cpp
@@ -0,0 +1,23 @@
+#include <iostream>  
+using namespace std;  
+class Employee  
+ {  
+   public:  
+        Employee()    
+        {    
+            cout<<"Constructor Invoked"<<endl;    
+        }    
+        ~Employee()    
+        {    
+            cout<<"Destructor Invoked"<<endl;    
+        }//creating   
+};  
+int main(void)   
+{  
+    Employee e1;   
+    Employee e2;   
+    return 0;  
+}  
+
+
+
