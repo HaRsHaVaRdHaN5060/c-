b83050c387ffd9c761f47b3eb7d9b9e5a826e847
HaRsHaVaRdHaN5060 <112234806+HaRsHaVaRdHaN5060@users.noreply.github.com>
2022-08-27 13:36:13 +0530

    Add files via upload

---
+++ program for default constructor.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class employee
+{
+ 	public:
+   		employee()
+   		{
+			cout<<"Default constructor invoked \n";
+   		}
+};
+int main()
+{
+  employee e1;
+  employee e2;
+  return 0;
+}  
+
+
